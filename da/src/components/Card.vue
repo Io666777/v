f2fe39c6d90c806e9d8a1e7ed368d067557acf37
Io666777 <dimaskitihin@gmail.com>
2025-05-14 08:14:54 +0000

    машина смерти заработала

--- da/src/components/Card.vue
+++ da/src/components/Card.vue
@@ -1,7 +1,5 @@
-<script setup>
-import { ref, inject } from 'vue'
-
-const props = defineProps({
+ <script setup>
+defineProps({
   id: Number,
   title: String,
   imageUrl: String,
@@ -9,40 +7,22 @@ const props = defineProps({
   isFavorite: Boolean,
   isAdded: Boolean,
   onClickFavorite: Function,
-  onClickAdd: Function,
-  description: String // Добавляем новое поле для описания
+  onClickAdd: Function
 })
-
-const { openProductDetail } = inject('productDetail')
-
-const handleClick = () => {
-  if (openProductDetail) {
-    openProductDetail({
-      id: props.id,
-      title: props.title,
-      imageUrl: props.imageUrl,
-      price: props.price,
-      description: props.description,
-      isFavorite: props.isFavorite,
-      isAdded: props.isAdded
-    })
-  }
-}
 </script>
 
 <template>
   <div 
     class="relative bg-white border border-slate-100 rounded-3xl p-6 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl flex flex-col"
     style="width: 300px; height: 400px; flex-shrink: 0;"
-    @click="handleClick"
   >
-    <!-- Кнопка избранного (теперь с stopPropagation) -->
+    <!-- Кнопка избранного -->
     <img
       v-if="onClickFavorite"
       :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
       alt="Like"
-      class="absolute top-6 left-6 z-10 w-6 h-6 hover:scale-110 transition-transform"
-      @click.stop="onClickFavorite"
+      class="absolute top-6 left-6 z-10 w-6 h-6"
+      @click="onClickFavorite"
     />
 
     <!-- Изображение товара -->
@@ -54,7 +34,7 @@ const handleClick = () => {
       />
     </div>
 
-    <!-- Название товара -->
+    <!-- Название товара с улучшенным отображением -->
     <div class="flex-grow min-h-[60px] mb-4 overflow-hidden">
       <p 
         class="text-base text-gray-900 font-medium line-clamp-2 break-words"
@@ -72,7 +52,7 @@ const handleClick = () => {
       </div>
       <img
         v-if="onClickAdd"
-        @click.stop="onClickAdd"
+        @click="onClickAdd"
         :src="!isAdded ? '/plus.svg' : '/checked.svg'"
         alt="Add to cart"
         class="w-8 h-8 hover:scale-110 transition-transform"
